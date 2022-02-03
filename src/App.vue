b02c0e57f0ea345e13e5522247ed01b5a4570bb4
RobinBas <basrobin77@gmail.com>
2022-02-03 17:45:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Boutton/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Boutton from './components/Boutton.vue'
+
+import './assets/styles/style.css';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Boutton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Boutton.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <button class="test">
+            test
+        </button>
+    </div>
+</template>
